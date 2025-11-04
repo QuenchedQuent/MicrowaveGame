d9c5df2e2387d74983c15a1261ea81b626c8799e
Quentin H <cat.fat30@yahoo.com>
2025-11-03 20:10:59 -0600

    Add rudimentary scoring

---
+++ Scenes/PassiveUpgrade/PassiveUpgrade.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class PassiveUpgrade : Button
+{
+	[Export] private Label _scoreLabel;
+	[Export] private Label _upgradeLabel;
+
+	private int _score = 0;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Disabled = true;
+
+		Pressed += OnPressed;
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+    {
+		_score = _scoreLabel.Text.ToInt();
+		ShouldButtonBeActive();
+    }
+	
+	private void ShouldButtonBeActive()
+	{
+		if (_score >= 10)
+		{
+			Disabled = false;
+		}
+		else
+        {
+			Disabled = true;
+        }
+    }
+
+    private void OnPressed()
+    {
+		_upgradeLabel.Text = (_upgradeLabel.Text.ToInt() + 1).ToString();
+		_scoreLabel.Text = (_score - 10).ToString();
+		_score = _scoreLabel.Text.ToInt();
+    }
+}
